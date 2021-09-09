f6120e91ef3a473ff6918c3037a6eb0a95ddfa5a
reyanRaj <72247352+reyanRaj@users.noreply.github.com>
2021-09-09 14:51:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ router.get('/',function(req,res){
 app.use('/',router);
 app.use(express.static(path.join(__dirname+'/public')));
 app.use(favicon(path.join(__dirname,'public','favicon.ico')));
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 console.log("Running at port 3000");
